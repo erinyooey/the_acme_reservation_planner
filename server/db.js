73cfb66495b79ba7d57de3305b736829eaaeabc1
Erin Yoo <yooeh@masters.edu>
2024-07-12 16:56:31 -0700

    implemented more

--- server/db.js
+++ server/db.js
@@ -3,9 +3,87 @@ const uuid = require("uuid")
 const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/acme_reservation_db")
 
 const createTables = async() => {
-    return "Does this work";
+    await client.connect()
+    const SQL = `
+        DROP TABLE IF EXISTS customers;
+        DROP TABLE IF EXISTS restaurants;
+        DROP TABLE IF EXISTS reservations;
+        CREATE TABLE customers(
+            id UUID PRIMARY KEY,
+            name VARCHAR(255) NOT NULL UNIQUE
+        );
+        CREATE TABLE restaurants(
+            id UUID PRIMARY KEY,
+            name VARCHAR(255) NOT NULL UNIQUE
+        );
+        CREATE TABLE reservations(
+            id UUID PRIMARY KEY,
+            date DATE NOT NULL,
+            party_count INTEGER NOT NULL,
+            restaurant_id UUID REFERENCES restaurants(id) NOT NULL,
+            customer_id UUID REFERENCES customer(id) NOT NULL
+            
+        );
+    `;
+    await client.query(SQL);
+
 };
 
+const createCustomer = async({name}) => {
+    // await client.connect();
+    const SQL = `
+        INSERT INTO customers(id, name) VALUES($1, $2) RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(),name])
+    return response.rows[0]
+}
+
+const createRestaurant = async({name }) => {
+    const SQL = `
+        INSERT INTO restaurants(id, name) VALUES($1, $2) RETURNING *
+    `
+    const response = await client.query(SQL, [uuid.v4(), name])
+    return response.rows[0]
+}
+
+const createReservation = async({date, party_count, restaurant_id, customer_id}) => {
+    const SQL = `
+        INSERT INTO reservations(id, date, party_count, restaurant_id, customer_id) VALUES($1, $2, $3, $4, $5) RETURNING *
+    `
+    const response = await client.query(SQL, [uuid.v4(), date, party_count, restaurant_id, customer_id])
+    return response.rows[0]
+}
+
+const fetchCustomers = async() => {
+    const SQL = `SELECT * FROM customer`
+    const response = await client.query(SQL)
+    return response.rows
+}
+
+const destroyReservation = async({restaurant_id, customer_id}) => {
+    console.log(restaurant_id, customer_id)
+    const SQL = `
+        DELETE FROM reservations WHERE restaurant_id = $1 AND customer_id = $2
+    `
+    await client.query(SQL, [restaurant_id, customer_id])
+}
+
+const fetchReservations = async()=>{
+    const SQL = `
+        SELECT * FROM reservations;
+    `
+    const response = await client.query(SQL)
+    return response.rows
+}
+
+
 module.exports = {
-    createTables
+    createTables,
+    createCustomer,
+    client,
+    createRestaurant,
+    createReservation,
+    fetchCustomers,
+    destroyReservation,
+    fetchReservations
 };
--- server/index.js
+++ server/index.js
@@ -3,13 +3,60 @@ const app = express()
 app.use(express.json())
 const PORT = process.env.PORT || 3000;
 
-const {createTables} = require("./db")
+const {
+    createTables,
+    createCustomer,
+    client,
+    createRestaurant,
+    createReservation,
+    fetchCustomers,
+    destroyReservation,
+    fetchReservations
+
+} = require("./db")
 
 const init = async() => {
-    const response = await createTables()
+    await client.connect()
+    // const response = await createTables()
     app.listen(PORT, ()=>{
         console.log(`Hello from port number ${PORT}`)
     })
 } 
 
-init()
+app.post("/api/customers/:id/reservations", async(req, res, next)=>{
+    try {
+        res.status(201).send(await createReservation({
+            customer_id: req. params.customer_id, restaurant_id: req.body.restaurant_id, date: req.body.date, party_count: req.body.party_count,
+        }))
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.get("/api/customers", async(req, res, next)=>{
+    try {
+        res.send(await fetchCustomers())
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.get("/api/restaurants", async(req, res, next)=>{
+    try {
+        res.send(await fetchReservations())
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.delete("/api/customers/:customer_id/reservations/:id", async(req, res, next)=>{
+    try {
+        await destroyReservation({customer_id: req.params.customer_id, id: req.params.id})
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+init();
